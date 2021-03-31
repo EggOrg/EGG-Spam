8802ec7e40c4b18b6ba75110ddf8d1110034d476
Unkwown-User2 <benjaminjneudorf@gmail.com>
2021-03-31 15:46:16 -0500

    added E V E R Y T H I N G

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# EGG Spam
- 
+<img src="https://img.shields.io/badge/EggOrg-unknown--user2-green?style=for-the-badge">
+<br><br>
+<h3>A little python spammer I made<br>you need to install <a href="https://pyautogui.readthedocs.io/en/latest/install.html">pyautogui</a></h3>
---
+++ spam.py
@@ -0,0 +1,20 @@
+import pyautogui
+import time
+
+msg = input("Enter the message: ")
+a = input("How many times ?: ")
+t = input("how many seconds between each message: ")
+
+
+for i in range(1, 6):
+    print(i)
+    i + 1
+    time.sleep(1)
+
+print("SPAM")
+
+for i in range(0,int(a)):
+    pyautogui.typewrite(msg + '\n')
+    time.sleep(float(t))
+
+print("Done!")
